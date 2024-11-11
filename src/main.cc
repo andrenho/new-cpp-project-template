9048e02e2ee3ef710c91381e1860890c2ee7eceb
Andre Wagner <andre.nho@gmail.com>
2024-11-11 07:51:16 -0600

    .

--- src/main.cc
+++ src/main.cc
@@ -9,8 +9,8 @@ int main()
     LuaEngine lua_engine;
     lua_engine.hello();
 
-    /*
     UI ui;
+    /*
     auto last_frame = hr::now();
     while (ui.running()) {
         auto new_frame = hr::now();
