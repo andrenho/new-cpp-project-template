5ebfbefeb31d0ebfe601f180c9a58b1d5315d3e6
Andre Wagner <andre.nho@gmail.com>
2024-11-10 18:52:07 -0600

    .

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "-r" ]; then
 	git submodule update --init --recursive
-    mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make
+    mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j10
 elif [ "$1" = "-d" ]; then
 	git submodule update --init --recursive
-    mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make
+    mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j10
 elif [ "$1" = "-c" ]; then
     rm -rf build
 else
